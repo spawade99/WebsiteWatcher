1f4754466987b6c03bacd11c05456291f45756b9
Shubham Pawade <Shubham.Pawade@bentley.com>
2025-05-01 21:48:22 +0530

    Remove Function1 and add Register for website handling
    This commit removes the `Function1.cs` file and introduces a new `Register.cs` file that implements an HTTP trigger function for handling POST requests. The new function logs the request, deserializes the request body into a `Website` object, and returns it with a newly assigned GUID. Additionally, a `Website` class is added to define the structure of the website data. The project file is updated to include a reference to `Microsoft.Azure.Functions.Worker.Extensions.Sql` for SQL output functionality, while existing Application Insights package references are commented out.

---
+++ WebsiteWatcher/Register.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace WebsiteWatcher;
+
+public class Register(ILogger<Register> logger)
+{
+    private readonly ILogger<Register> _logger = logger;
+
+    [Function(nameof(Register))]
+    [SqlOutput("dbo.Websites","WebsiteWatcher")]
+    public async Task<Website> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var requestBody= await new StreamReader(req.Body).ReadToEndAsync();
+        var options= new JsonSerializerOptions() { PropertyNameCaseInsensitive=false };
+        var newWebsite = JsonSerializer.Deserialize<Website>(requestBody, options);
+
+        newWebsite.Id = Guid.NewGuid();
+
+        return newWebsite;
+    }
+}
+
+public class Website
+{
+    public Guid Id { get; set; }
+    public string Url { get; set; }
+    public string? XPathExpression { get; set; }
+}
